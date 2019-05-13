4c13b622fc4f8d3893a49f2f582b4c455b633199
Dan Balarin <dan.balarin@gmail.com>
2019-05-13 16:58:09 +0200

    seting up enviroment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
+const path = require('path');
 const withSass = require('@zeit/next-sass');
 const withPlugins = require('next-compose-plugins');
 
-const nextConfig = {target: 'serverless'};
+const absolutePaths = ['components', 'styles'];
+
+const nextConfig = {
+    target: 'serverless',
+    webpack (config, options) {
+        absolutePaths.forEach(val => {
+            config.resolve.alias[val] = path.join(__dirname, val)
+        })
+        return config
+    }
+};
 
 module.exports = withPlugins([
     [withSass]
---
+++ pages/test.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {Button} from 'reactstrap'
+
+export class test extends Component {
+  render() {
+    return (
+      <div>
+        <Button color="primary">test</Button>
+      </div>
+    )
+  }
+}
+
+export default test
